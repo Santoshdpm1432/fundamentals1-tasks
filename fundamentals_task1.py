f033115c57531ea75b2e2f62294cf7e3e5dd2bd0
santosh kumar agepogu <santoshkumaragepogu484@gmail.com>
2025-03-17 01:04:59 -0600

    Add files via upload

---
+++ fundamentals_task1.py
@@ -0,0 +1,63 @@
+#day_1 task:
+
+'''Create a Python script with both single-line and multi-line comments
+explaining the purpose of the script'''
+# television = 20000 #television cost is 20000
+# iphone = 120000 # iphone cost is 120000
+# refridrator = 20000 # refridrator cost is 20000
+# total_cost = television + iphone + refridrator 
+# print(total_cost)
+'''i purchased television,
+iphone&refridrator today the total cost of today's shopping is 
+i will mention below'''
+
+'''Declare two variables, one storing an integer and the other a 
+string. Print their values'''
+
+product_name = ("my roadstar jacket cost ")
+cost = 2000
+print(product_name)
+print(cost)
+
+
+#Write a program that prints a pattern using multiple print statements.
+letter_1 = "pattern"
+print(letter_1)
+print(letter_1,letter_1)
+print(letter_1,letter_1,letter_1)
+
+'''Create a Python script for a simple task and add comments to explain each
+step'''
+
+
+# television = 20000 #television cost is 20000
+# iphone = 120000 # iphone cost is 120000
+# refridrator = 20000 # refridrator cost is 20000
+# total_cost = television + iphone + refridrator 
+# print(total_cost)
+'''i purchased television,
+iphone&refridrator today the total cost of today's shopping is 
+i will '''
+
+#Create variables of different data types(int,float,str) and print their values.
+
+# number_1 = 1020
+# height = 5.7
+# username_1 = "santosh1432"
+
+
+#Determine the data type of a variable.
+#Expected Output:
+# The data type of variable x is <class 'int'>.
+
+# x = 1000
+# print(type(x))
+
+#Display the memory addresses
+# x = 10
+# y = 10
+# x = 10
+# y = 10
+# print(id(x))
+# print(id(y))
+
